a27fd6cec6ccbae65359a03c831feddfd108f949
felodiud <127905243+felodiud@users.noreply.github.com>
2023-05-24 01:35:52 +0000

    partiendo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//window.onload = function() {}
+//write your code here
+const body = document.body;
+const pcard = document.createElement("DIV");
+pcard.classList.add("card");
+body.append(pcard);
+
+const h1 = document.createElement("H1");
+pcard.append(h1);
+h1.innerHTML = "3";
